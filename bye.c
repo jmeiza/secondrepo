1c46346807480fca8ad164ab15d0f9ba0360e0c5
johnmoses <jmoses1519@gmail.com>
2024-10-02 11:52:01 -0400

    including bye.c

---
+++ bye.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main {
+	int length = 5;
+	int breadth = 4;
+
+	area = length * breadth;
+	printf("The area of the rectangle is %d",area);
+	return 0;
+}
