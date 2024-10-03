f5f4c8ebf1d267cec69488beda145bf4785c7092
johnmoses <jmoses1519@gmail.com>
2024-10-03 14:56:59 -0400

    Used printf

--- hello.c
+++ hello.c
@@ -5,6 +5,6 @@ int main(void) {
         puts("Hello World");
         puts("Nice to meet you world");
 	puts("Today is Thursday");
-	puts("Tomorrow will be Friday");
+	printf("Tomorrow will be Friday");
         return EXIT_SUCCESS;
 }
